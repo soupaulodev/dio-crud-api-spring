c781f7eeed3ed98d6b74aa4f7af615399aeb0f47
Paulo Marques <paulomarques.rs1@gmail.com>
2024-06-27 14:48:50 -0300

    Configuracoes para deploy

--- src/main/java/br/com/soupaulodev/dio_crud_api_spring/DioCrudApiSpringApplication.java
+++ src/main/java/br/com/soupaulodev/dio_crud_api_spring/DioCrudApiSpringApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DioCrudApiSpringApplication {
 
+	public String PORT = System.getenv("PORT");
+
 	public static void main(String[] args) {
 		SpringApplication.run(DioCrudApiSpringApplication.class, args);
 	}
