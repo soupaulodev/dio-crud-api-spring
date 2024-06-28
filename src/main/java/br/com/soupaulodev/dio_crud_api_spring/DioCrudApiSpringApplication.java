21461eacf972af300564a71e29d001909071a079
Paulo Marques <paulomarques.rs1@gmail.com>
2024-06-27 21:57:27 -0300

    fix: Correcao de cors

--- src/main/java/br/com/soupaulodev/dio_crud_api_spring/DioCrudApiSpringApplication.java
+++ src/main/java/br/com/soupaulodev/dio_crud_api_spring/DioCrudApiSpringApplication.java
@@ -3,6 +3,11 @@ package br.com.soupaulodev.dio_crud_api_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+
 @SpringBootApplication
 public class DioCrudApiSpringApplication {
 
